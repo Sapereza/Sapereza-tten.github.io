a4f03feee7cbc381f7583abc80ad41efd0f2224a
Sapereza <s.perezanderson@gmail.com>
2022-07-19 12:03:13 -0400

    the uppercase/lowercase/and capitalize not working yet

--- texted.js
+++ texted.js
@@ -3,5 +3,39 @@ function bold(){
 }
 
 function italic(){
-    document.getElementById("textarea").style.fontStyle="italic";
+    document.getElementById("textarea").style.fontStyle = "italic";
+}
+
+function aleft(){
+    document.getElementById("textarea").style.textAlign = "left";
+
+}
+
+function aright(){
+    document.getElementById("textarea").style.textAlign = "right";
+
+}
+
+function center(){
+    document.getElementById("textarea").style.textAlign = "center";
+
+}
+
+function uppercase(){
+    document.getElementById("textarea").style.fontWeight = "uppercase";
+
+
+}
+function lowercase(){
+    document.getElementById("textarea").style.fontWeight = "lowercase";
+
+}
+function capitalize(){
+    document.getElementById("textarea").style.font = "capitalize";
+
+
+}
+function clearScreen(){
+    document.getElementById("textarea").value=""
+
 }
